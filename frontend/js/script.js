c1f0b1890b88c98eb1f95d661e7a6f11547597c4
Kamylla <toddyaust@gmail.com>
2025-05-14 21:06:48 -0300

    Initial commit

---
+++ frontend/js/script.js
@@ -0,0 +1,107 @@
+// login elements
+
+const login = document.querySelector(".login")
+const loginForm = login.querySelector(".login-form")
+const loginInput = login.querySelector(".login-input")
+
+// chat elements
+
+const chat = document.querySelector(".chat")
+const chatForm = chat.querySelector(".chat-form")
+const chatInput = chat.querySelector(".chat-input")
+const chatMessages = chat.querySelector(".chat-messages")
+
+const user = {id: "", name: "", color: ""}
+
+const colors = [
+    "cadeblue",
+    "darkgoldenrod",
+    "darkkhaki",
+    "hotpink",
+    "gold",
+    "white"
+]
+
+let websocket
+
+const creatMessageSelfElement = (content) => {
+    const div = document.createElement("div")
+
+    div.classList.add("message-self")
+    div.innerHTML = content
+
+    return div
+}
+
+const creatMessageOtherElement = (content, sender, senderColor) => {
+    const div = document.createElement("div")
+    const span = document.createElement("span")
+
+    div.classList.add("message-other")
+
+    div.classList.add("message-self")
+
+    span.classList.add("message-sender")
+
+    span.style.color = senderColor
+
+    div.appendChild(span)
+
+    span.innerHTML = sender
+    div.innerHTML += content
+
+    return div
+}
+
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+}
+
+const scrollScreen = () => {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: "smooth"
+    })
+}
+
+const processMessage = ({ data }) => {
+    const { userId, userName, userColor, content } = JSON.parse(data)
+
+    const message = userId == user.id ? creatMessageSelfElement(content) : creatMessageOtherElement(content, userName, userColor)
+
+    chatMessages.appendChild(message)
+
+    scrollScreen()
+}
+
+const handleLogin = (event) => {
+    event.preventDefault()
+
+    user.id = crypto.randomUUID()
+    user.name = loginInput.value
+    user.color = getRandomColor()
+
+    login.style.display = "none"
+    chat.style.display = "flex"
+
+    websocket = new WebSocket("ws:localhost:8080")
+    websocket.onmessage = processMessage
+}
+
+const sendMenssage = (event) => {
+    event.preventDefault()
+
+    const message = {
+        userId: user.id,
+        userName: user.name,
+        userColor: user.color,
+        content: chatInput.value
+    }
+    websocket.send(JSON.stringify(message))
+
+    chatInput.value = ""
+}
+
+loginForm.addEventListener("submit", handleLogin)
+chatForm.addEventListener("submit", sendMenssage)
